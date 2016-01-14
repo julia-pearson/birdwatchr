2962d9a5d3990afa7f6d5ea49ccb575ee9ea57a3
Julia Pearson <julia.pearson@gmail.com>
2016-01-14 09:04:42 -0500

    homework

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,11 @@ console.log("        <')_,/ ")
 console.log("        (_==/  ")
 console.log("birder  ='-    ")
 console.log("we're hiring!  ")
+
+$(document).ready(function() {
+  var birdLink = $("#see-the-birds");
+  birdLink.on('click', function(){
+    preventDefault();
+    // ajax request to get list of birds
+  });
+});
